600b32314ab41a60c2102b302d352348064c30af
Jesper Nyström <jesper.nystrom@live.se>
2018-04-17 12:59:13 +0200

    Add css buttons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as angular from 'angular';
 let app = angular.module('awesomeApp', []);
 
 app.controller('awesomeAppController', function TodoCtrl($scope) {
+    $scope.object = 
     $scope.todos = [
         //{text:'', done:false},         
         //{text:'', done:false}
@@ -11,13 +12,17 @@ app.controller('awesomeAppController', function TodoCtrl($scope) {
     $scope.getTotalTodos = function () {
         return $scope.todos.length;
       };
+
+     /*  $scope.addStep = function() {
+        $scope.
+      } */
       $scope.addTodo = function () {
         $scope.todos.push({text:$scope.formTodoText, done:false});
         $scope.formTodoText = '';
       };
       
         $scope.clearCompleted = function () {
-            $scope.todos = $scope.filter($scope.todos, function(todo){
+            $scope.todos =($scope.todos, function(todo){
                 return !todo.done;
             });
         };
