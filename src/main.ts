4c9f8dce19f56fb2a3c21c2bd47abebd4244ce56
Jesper Nyström <jesper.nystrom@live.se>
2018-04-17 11:24:41 +0200

    Add General functionality

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,33 @@ import * as angular from 'angular';
 
 let app = angular.module('awesomeApp', []);
 
-app.controller('awesomeAppController', function($scope) {
-        $scope.message = "Hello World!";
+app.controller('awesomeAppController', function TodoCtrl($scope) {
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false},         
+        {text: 'Build an app', done:false}
+      ];
+       
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+      };
+      $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+      };
+      
+        $scope.clearCompleted = function () {
+            $scope.todos = $scope.filter($scope.todos, function(todo){
+                return !todo.done;
+            });
+        };
+      
 } );
+
+  
+    
+   
+    
+   
+  
 
 // ...and so it begins... 
