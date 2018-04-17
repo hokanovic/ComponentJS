14d0b02720e722cca4057898862290ead35c6ebf
Jesper Nyström <jesper.nystrom@live.se>
2018-04-17 11:01:03 +0200

    Add controler class

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import * as angular from 'angular';
 
 let app = angular.module('awesomeApp', []);
 
+app.controller('awesomeAppController', function($scope) {
+        $scope.message = "Hello World!";
+} );
+
 // ...and so it begins... 
